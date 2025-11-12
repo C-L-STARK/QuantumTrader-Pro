a92fd948ec5c9a772c56fffb2ade2bb69bb6ca9d
Dezirae Stark <clockwork.halo@tutanota.de>
2025-11-12 00:26:05 -0500

    fix: Add missing Flutter material import for Color class

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
 enum TradingMode { conservative, aggressive }
 enum TrendDirection { bullish, bearish, neutral }
